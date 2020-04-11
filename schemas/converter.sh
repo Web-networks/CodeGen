a8852304865df74d5735552d7d17f3e4286c76f6
Roman Nikonov <niconneo@yandex.ru>
2020-04-11 17:27:23 +0300

    move schemas

---
+++ schemas/converter.sh
@@ -0,0 +1,12 @@
+# sed -re 's/^( *)#"([a-z_]*)": "([a-z_]*)".{,2}$/\1\2:\n\1  type: \3/g' definitions.yaml > definitions.yaml.new
+
+cat definitions.yaml \
+  | sed -re 's/^( *)type: float/\1type: number\n\1format: float/g' \
+  | sed -re 's!type: regularizer!$ref: '\''#/components/schemas/Regularizer'\''!g' \
+  | sed -re 's!type: regulizer!$ref: '\''#/components/schemas/Regularizer'\''!g' \
+  | sed -re 's!type: activations?!$ref: '\''#/components/schemas/Activation'\''!g' \
+  | sed -re 's!type: constraints?!$ref: '\''#/components/schemas/Constraint'\''!g' \
+  | sed -re 's!type: initializers?!$ref: '\''#/components/schemas/Initializers'\''!g' \
+  > definitions.yaml.new
+
+diff --color=always -u definitions.yaml definitions.yaml.new #| less -FR
