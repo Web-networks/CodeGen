60a56a21345db04e263338991cb5090a8c8dd20f
Roman Nikonov <niconneo@yandex.ru>
2020-06-07 13:02:27 +0300

    update generated code

--- sample_few_layers/generated/model.py
+++ sample_few_layers/generated/model.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Conv2D
 
 
 def init_model():
