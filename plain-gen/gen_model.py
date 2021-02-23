87a5eec05304c7183553dcf14bd4c6c00091bf16
Ekaterina Kulagina <Katrin.Kulagina97@yandex.ru>
2021-02-23 21:50:21 +0300

    add model file generation

---
+++ plain-gen/gen_model.py
@@ -0,0 +1,45 @@
+import json
+
+def read_input(filename):
+    with open('model.json') as json_file:
+        data = json.load(json_file)
+        return data
+
+def create_list_imports(json_data):
+    layers = set()
+    layers_info = json_data["layers"]
+    for item in layers_info:
+        layers.add(item["type"])
+    return layers
+
+def create_model_imports_file(json_data, layers):
+    output = "from tensorflow.keras.models import Sequential\n"
+    for item in layers:
+        output = output + "from tensorflow.keras.models import {}\n".format(item)
+    return output
+
+def create_init_model_func(json_data):
+    output = "def init_model():\n    model = Sequential()\n"
+    for item in json_data["layers"]:
+        output = output + "    model.add({}(".format(item["type"])
+        if "params" not in item:
+            output = output + "))\n"
+        else:
+            for key, value in item["params"].items():
+                if type(value) is str:
+                    output = output + "{}=\"{}\",".format(key,value)
+                else:
+                    output = output + "{}={},".format(key,value)
+            output = output + ")\n"
+    output = output + "    return model"
+    return output
+
+
+inp = read_input("model.json")
+l = create_list_imports(inp)
+imports = create_model_imports_file(inp, l)
+result = create_init_model_func(inp)
+to_file = imports + "\n" + result
+f = open('model.py', 'w')
+f.write(to_file)
+f.close()
